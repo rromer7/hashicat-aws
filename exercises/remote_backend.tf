0b166c81b857e9e9a4f37824302f11e4769517b0
root <rromer7@bancodebogota.com.cogit config --global user.name Sucy Romero>
2022-10-06 21:46:13 +0000

    Added remote backend

--- exercises/remote_backend.tf
+++ exercises/remote_backend.tf
@@ -5,7 +5,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "YOURORGANIZATION"
+    organization = "bdb"
     workspaces {
       name = "YOURWORKSPACE"
     }
